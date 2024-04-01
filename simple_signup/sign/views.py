5c64240ed8e55cfc73773275643fae58c47dbc44
xRuineRx <115977690+xRuineRx@users.noreply.github.com>
2024-04-01 18:10:13 +0300

    Add files via upload

---
+++ simple_signup/sign/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
+from .models import BaseRegisterForm
+from django.shortcuts import redirect
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+class IndexView(LoginRequiredMixin,TemplateView):
+    template_name = 'protect/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_premium'] = not self.request.user.groups.filter(name= 'Premium').exists()
+        return context
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    premium_group = Group.objects.get(name='Premium')
+    if not request.user.groups.filter(name='Premium').exists():
+        premium_group.user_set.add(user)
+    return redirect('/')
+
+
+class BaseRegisterView(CreateView):
+    model = User
+    form_class = BaseRegisterForm
+    success_url = '/'
